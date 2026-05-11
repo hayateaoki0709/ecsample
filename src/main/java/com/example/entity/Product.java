5861ee9492c11812a4f5af2692bfa64c570af5a1
hashi-h <hiroki.hashi520@gmail.com>
2026-05-11 11:15:12 +0900

    MyBatisでProductをDBから取得するように変更

--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -5,12 +5,6 @@ public class Product {
 	private String name;
 	private int price;
 
-	public Product(int id, String name, int price) {
-		this.id = id;
-		this.name = name;
-		this.price = price;
-	}
-
 	public int getId() {
 		return id;
 	}
